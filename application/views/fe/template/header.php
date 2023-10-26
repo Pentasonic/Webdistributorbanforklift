2c5bc890245270558b3470249b31a622a2c4c3a9
Doni Nurramdan <nurramdandoni@gmail.com>
2023-10-26 16:14:12 +0700

    update header mobile

--- application/views/fe/template/header.php
+++ application/views/fe/template/header.php
@@ -105,13 +105,13 @@
             <div class="rsnp-mnu">
                 <span class="rspn-mnu-cls"><i class="fa fa-times"></i></span>
                 <ul class="mb-0 list-unstyled w-100">
-                    <li><a href="<?php echo base_url() ?>" title="">HOME</a></li>
-                    <li><a href="<?php echo base_url() ?>" title="">BAN FORKLIFT</a></li>
-                    <li><a href="<?php echo base_url() ?>" title="">BAN LOADER</a></li>
-                    <li><a href="<?php echo base_url() ?>" title="">BAN TRUCK</a></li>
-                    <li><a href="<?php echo base_url() ?>" title="">MOBILE PRESS BAN</a></li>
-                    <li><a href="<?php echo base_url() ?>" title="">ARTIKEL</a></li>
-                    <li><a href="<?php echo base_url() ?>" title="">HUBUNGI KAMI</a></li>
+                <li><a href="<?php echo base_url() ?>" title="">HOME</a></li>
+                <li><a href="<?php echo base_url() ?>forklift" title="">BAN FORKLIFT</a></li>
+                <li><a href="<?php echo base_url() ?>loader" title="">BAN LOADER</a></li>
+                <li><a href="<?php echo base_url() ?>truck" title="">BAN TRUCK</a></li>
+                <li><a href="<?php echo base_url() ?>press" title="">MOBILE PRESS BAN</a></li>
+                <li><a href="<?php echo base_url() ?>artikel" title="">ARTIKEL</a></li>
+                <li><a href="<?php echo base_url() ?>hubungi" title="">HUBUNGI KAMI</a></li>
                 </ul>
             </div><!-- Responsive Menu -->
         </div><!-- Responsive Header --> <!-- END of HEADER -->
